1ac4612067f067093c3b77ba496d6de84d3790e6
Harm <el.harmo@gmail.com>
2019-02-16 15:04:29 +0100

    update to more rustish

--- src/main.rs
+++ src/main.rs
@@ -4,118 +4,97 @@ use std::io::Write;
 use std::process;
 
 enum Registers {
-    R_R0 = 0,
-    R_R1,
-    R_R2,
-    R_R3,
-    R_R4,
-    R_R5,
-    R_R6,
-    R_R7,
-    R_PC, /* program counter */
-    R_COND,
-    R_COUNT,
+    R0 = 0,
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    ProgramCounter,
+    Condition,
+    Count,
 }
 
-enum opcodes {
-    OP_BR = 0,
-    /* branch */
-    OP_ADD,
-    /* add  */
-    OP_LD,
-    /* load */
-    OP_ST,
-    /* store */
-    OP_JSR,
-    /* jump register */
-    OP_AND,
-    /* bitwise and */
-    OP_LDR,
-    /* load register */
-    OP_STR,
-    /* store register */
-    OP_RTI,
-    /* unused */
-    OP_NOT,
-    /* bitwise not */
-    OP_LDI,
-    /* load indirect */
-    OP_STI,
-    /* store indirect */
-    OP_JMP,
-    /* jump */
-    OP_RES,
-    /* reserved (unused) */
-    OP_LEA,
-    /* load effective address */
-    OP_TRAP,
-    /* execute trap */
+enum OperationCodes {
+    Branch = 0,
+    Add,
+    Load,
+    Store,
+    JumpRegister,
+    And,
+    LoadRegister,
+    StoreRegister,
+    Unused,
+    Not,
+    LoadIndirect,
+    StoreIndirect,
+    Jump,
+    Reserved,
+    LoadEffectiveAddress,
+    Trap,
 }
 
-impl opcodes {
-    fn from_integer(x: u16) -> opcodes {
-        unsafe { std::mem::transmute::<u8, opcodes>(x as u8) }
+impl OperationCodes {
+    fn from_integer(x: u16) -> OperationCodes {
+        unsafe { std::mem::transmute::<u8, OperationCodes>(x as u8) }
     }
 }
 
-enum trap_codes {
-    TRAP_GETC = 0x20,  /* get character from keyboard */
-    TRAP_OUT = 0x21,   /* output a character */
-    TRAP_PUTS = 0x22,  /* output a word string */
-    TRAP_IN = 0x23,    /* input a string */
-    TRAP_PUTSP = 0x24, /* output a byte string */
-    TRAP_HALT = 0x25,  /* halt the program */
+enum TrapCodes {
+    GetCharacter = 0x20,
+    Out = 0x21,
+    Puts = 0x22,
+    TRAP_IN = 0x23,
+    PutsTwo = 0x24,
+    Halt = 0x25,
 }
 
-impl trap_codes {
-    fn from_integer(x: u16) -> trap_codes {
-        unsafe { std::mem::transmute::<u8, trap_codes>(x as u8) }
+impl TrapCodes {
+    fn from_integer(x: u16) -> TrapCodes {
+        unsafe { std::mem::transmute::<u8, TrapCodes>(x as u8) }
     }
 }
 
-enum condition_flags {
-    FL_POS = 1 << 0,
-    /* P */
-    FL_ZRO = 1 << 1,
-    /* Z */
-    FL_NEG = 1 << 2,
-    /* N */
+enum ConditionFlags {
+    Positive = 1 << 0,
+    Zero = 1 << 1,
+    Negative = 1 << 2,
 }
 
 struct VM {
     memory: [u16; std::u16::MAX as usize],
-    reg: [u16; Registers::R_COUNT as usize],
+    reg: [u16; Registers::Count as usize],
 }
 
 impl VM {
     fn start(&mut self) {
-        enum position {
-            PC_START = 0x3000,
-        };
+        let StartPosition: u16 = 0x3000;
 
-        self.reg[Registers::R_PC as usize] = position::PC_START as u16;
+        self.reg[Registers::ProgramCounter as usize] = StartPosition;
 
         let running = true;
         while running {
             /* FETCH */
-            let instr = self.mem_read(self.reg[Registers::R_PC as usize]);
+            let instr = self.mem_read(self.reg[Registers::ProgramCounter as usize]);
             let op = instr >> 12;
-            self.reg[Registers::R_PC as usize] += 1; // Post increment program counter
-            match opcodes::from_integer(op) {
-                opcodes::OP_ADD => self.add(op),
-                opcodes::OP_AND => self.and(op),
-                opcodes::OP_NOT => self.not(op),
-                opcodes::OP_BR => self.br(op),
-                opcodes::OP_JMP => self.jmp(op),
-                opcodes::OP_JSR => self.jsr(op),
-                opcodes::OP_LD => self.ld(op),
-                opcodes::OP_LDI => self.ldi(op),
-                opcodes::OP_LDR => self.ldr(op),
-                opcodes::OP_LEA => self.lea(op),
-                opcodes::OP_ST => self.st(op),
-                opcodes::OP_STI => self.sti(op),
-                opcodes::OP_STR => self.str(op),
-                opcodes::OP_TRAP => self.trap(op),
+            self.reg[Registers::ProgramCounter as usize] += 1; // Post increment program counter
+            match OperationCodes::from_integer(op) {
+                OperationCodes::Add => self.add(op),
+                OperationCodes::And => self.and(op),
+                OperationCodes::Not => self.not(op),
+                OperationCodes::Branch => self.branch(op),
+                OperationCodes::Jump => self.jump(op),
+                OperationCodes::JumpRegister => self.jump_register(op),
+                OperationCodes::Load => self.load(op),
+                OperationCodes::LoadIndirect => self.load_indirect(op),
+                OperationCodes::LoadRegister => self.load_register(op),
+                OperationCodes::LoadEffectiveAddress => self.load_effective_address(op),
+                OperationCodes::Store => self.store(op),
+                OperationCodes::StoreIndirect => self.store_indirect(op),
+                OperationCodes::StoreRegister => self.store_register(op),
+                OperationCodes::Trap => self.trap(op),
                 _ => (),
             }
         }
@@ -167,64 +146,65 @@ impl VM {
         self.update_flags(r0);
     }
 
-    fn br(&mut self, instr: u16) {
+    fn branch(&mut self, instr: u16) {
         let pc_offset = sign_extend((instr) & 0x1ff, 9);
         let cond_flag = (instr >> 9) & 0x7;
-        if cond_flag & self.reg[Registers::R_COND as usize] > 0 {
-            self.reg[Registers::R_PC as usize] += pc_offset;
+        if cond_flag & self.reg[Registers::Condition as usize] > 0 {
+            self.reg[Registers::ProgramCounter as usize] += pc_offset;
         }
     }
 
-    fn jmp(&mut self, instr: u16) {
+    fn jump(&mut self, instr: u16) {
         /* Also handles RET */
         let r1 = (instr >> 6) & 0x7;
-        self.reg[Registers::R_PC as usize] = self.reg[r1 as usize];
+        self.reg[Registers::ProgramCounter as usize] = self.reg[r1 as usize];
     }
 
-    fn jsr(&mut self, instr: u16) {
+    fn jump_register(&mut self, instr: u16) {
         let jsr = (instr >> 11) & 1;
         if jsr > 0 {
             let pc_offset = sign_extend(instr & 0x7FF, 11);
-            self.reg[Registers::R_PC as usize] += pc_offset;
+            self.reg[Registers::ProgramCounter as usize] += pc_offset;
         } else {
             //jsrr
-            self.reg[Registers::R_PC as usize] = (instr >> 6) & 0x7;
+            self.reg[Registers::ProgramCounter as usize] = (instr >> 6) & 0x7;
         }
     }
 
-    fn ld(&mut self, instr: u16) {
+    fn load(&mut self, instr: u16) {
         /* destination register (DR) */
         let r0 = (instr >> 9) & 0x7;
         /* PCoffset 9*/
         let pc_offset = sign_extend(instr & 0x1ff, 9);
         /* add pc_offset to the current PC, look at that memory location to get the final address */
-        self.reg[r0 as usize] = self.mem_read(self.reg[Registers::R_PC as usize] + pc_offset);
+        self.reg[r0 as usize] =
+            self.mem_read(self.reg[Registers::ProgramCounter as usize] + pc_offset);
         self.update_flags(r0);
     }
 
     fn update_flags(&mut self, r: u16) {
         let r_val = self.reg[r as usize];
-        self.reg[Registers::R_COND as usize] = if r_val > 0 {
-            condition_flags::FL_ZRO as u16
+        self.reg[Registers::Condition as usize] = if r_val > 0 {
+            ConditionFlags::Zero as u16
         } else if (r_val >> 15) > 0 {
-            condition_flags::FL_NEG as u16
+            ConditionFlags::Negative as u16
         } else {
-            condition_flags::FL_POS as u16
+            ConditionFlags::Positive as u16
         }
     }
 
-    fn ldi(&mut self, instr: u16) {
+    fn load_indirect(&mut self, instr: u16) {
         /* destination register (DR) */
         let r0 = (instr >> 9) & 0x7;
         /* PCoffset 9*/
         let pc_offset = sign_extend(instr & 0x1ff, 9);
         /* add pc_offset to the current PC, look at that memory location to get the final address */
-        let read = self.mem_read(self.reg[Registers::R_PC as usize] + pc_offset);
+        let read = self.mem_read(self.reg[Registers::ProgramCounter as usize] + pc_offset);
         self.reg[r0 as usize] = self.mem_read(read);
         self.update_flags(r0);
     }
 
-    fn ldr(&mut self, instr: u16) {
+    fn load_register(&mut self, instr: u16) {
         // 0x40 ? 64 / 16 =  4 ???
         let offset: u16 = sign_extend(instr & 0b11_1111, 6);
 
@@ -235,35 +215,35 @@ impl VM {
         self.update_flags(dr);
     }
 
-    fn lea(&mut self, instr: u16) {
+    fn load_effective_address(&mut self, instr: u16) {
         /* destination register (DR) */
         let r0 = (instr >> 9) & 0x7;
         /* PCoffset 9*/
         let pc_offset = sign_extend(instr & 0x1ff, 9);
         /* add pc_offset to the current PC, look at that memory location to get the final address */
-        self.reg[r0 as usize] = self.reg[Registers::R_PC as usize] + pc_offset;
+        self.reg[r0 as usize] = self.reg[Registers::ProgramCounter as usize] + pc_offset;
         self.update_flags(r0);
     }
 
-    fn st(&mut self, instr: u16) {
+    fn store(&mut self, instr: u16) {
         let sr = (instr >> 9) & 0x7;
         /* PCoffset 9*/
         let pc_offset = sign_extend(instr & 0x1ff, 9);
         self.mem_write(
-            self.reg[Registers::R_PC as usize] + pc_offset,
+            self.reg[Registers::ProgramCounter as usize] + pc_offset,
             self.reg[sr as usize],
         );
     }
 
-    fn sti(&mut self, instr: u16) {
+    fn store_indirect(&mut self, instr: u16) {
         let sr = (instr >> 9) & 0x7;
         /* PCoffset 9*/
         let pc_offset = sign_extend(instr & 0x1ff, 9);
-        let read = self.mem_read(self.reg[Registers::R_PC as usize] + pc_offset);
+        let read = self.mem_read(self.reg[Registers::ProgramCounter as usize] + pc_offset);
         self.mem_write(read, self.reg[sr as usize]);
     }
 
-    fn str(&mut self, instr: u16) {
+    fn store_register(&mut self, instr: u16) {
         let sr = (instr >> 9) & 0x7;
         /* PCoffset 6*/
         let offset: u16 = sign_extend(instr & 0b11_1111, 6);
@@ -272,13 +252,13 @@ impl VM {
     }
 
     fn trap(&mut self, instr: u16) {
-        match trap_codes::from_integer(instr & 0xFF) {
-            trap_codes::TRAP_GETC => self.get_character(),
-            trap_codes::TRAP_OUT => self.out(),
-            trap_codes::TRAP_PUTS => self.puts(),
-            trap_codes::TRAP_IN => self.scan(),
-            trap_codes::TRAP_PUTSP => self.putsp(),
-            trap_codes::TRAP_HALT => self.halt()
+        match TrapCodes::from_integer(instr & 0xFF) {
+            TrapCodes::GetCharacter => self.get_character(),
+            TrapCodes::Out => self.out(),
+            TrapCodes::Puts => self.puts(),
+            TrapCodes::TRAP_IN => self.scan(),
+            TrapCodes::PutsTwo => self.putsp(),
+            TrapCodes::Halt => self.halt(),
         }
     }
 
@@ -290,16 +270,16 @@ impl VM {
             .map(|byte| byte as u16)
             .expect("Could not read character!");
 
-        self.reg[Registers::R_R0 as usize] = input & 0b1111_1111; 
+        self.reg[Registers::R0 as usize] = input & 0b1111_1111;
     }
 
-    fn out(&mut self){
-        print!("{}", self.reg[Registers::R_R0 as usize] as u8 as char);
+    fn out(&mut self) {
+        print!("{}", self.reg[Registers::R0 as usize] as u8 as char);
         stdout().flush().expect("Could not print!");
     }
 
     fn puts(&mut self) {
-        let mut addr = self.reg[Registers::R_R0 as usize];
+        let mut addr = self.reg[Registers::R0 as usize];
         let mut character = self.memory[addr as usize];
 
         while character != 0 {
@@ -311,31 +291,31 @@ impl VM {
     }
 
     // in
-    fn scan(&mut self){
+    fn scan(&mut self) {
         print!("Enter a character: ");
         stdout().flush().expect("Could not print!");
 
         self.get_character();
     }
 
-    fn putsp(&mut self){
-        let mut addr = self.reg[Registers::R_R0 as usize];
+    fn putsp(&mut self) {
+        let mut addr = self.reg[Registers::R0 as usize];
         let mut character = self.memory[addr as usize];
 
         while character != 0 {
             print!("{}", (character & 0b1111_1111) as u8 as char);
             let second_part = (character >> 8) & 0b1111_1111;
-            if second_part == 0{
+            if second_part == 0 {
                 break;
             }
             print!("{}", second_part as u8 as char);
             addr = addr + 1;
             character = self.memory[addr as usize];
         }
-        stdout().flush().expect("Could not print!"); 
+        stdout().flush().expect("Could not print!");
     }
 
-    fn halt(&mut self){
+    fn halt(&mut self) {
         println!("Goodbye!");
         process::exit(0);
     }
@@ -354,7 +334,7 @@ fn main() {
     //  {Load Arguments, 12}
     //     {Setup, 12}
     let mut vm = VM {
-        reg: [0; Registers::R_COUNT as usize],
+        reg: [0; Registers::Count as usize],
         memory: [0; std::u16::MAX as usize],
     };
     vm.start();
